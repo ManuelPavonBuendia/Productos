2dc3f370d2adb1346722cd6e463f1f14761b4fd3
ManuelPavonBuendia <manuel.pavonbuendia@educa.madrid.org>
2026-01-29 21:35:18 +0100

    ejericico con listview

---
+++ app/src/main/java/es/etg/pmdm/productos/ProductoconProveedor.kt
@@ -0,0 +1,8 @@
+package es.etg.pmdm.productos
+
+data class ProductoConProveedor(
+    val nombreProducto: String,
+    val descripcion: String,
+    val nombreProveedor: String
+)
+
--- app/src/main/java/es/etg/pmdm/productos/ui/MainActivity.kt
+++ app/src/main/java/es/etg/pmdm/productos/ui/MainActivity.kt
@@ -1,9 +1,16 @@
 package es.etg.pmdm.productos.ui
 
+import ProductoAdapter
+import android.content.Context
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.room.Room
+import es.etg.pmdm.productos.ProductoConProveedor
 import es.etg.pmdm.productos.data.local.database.ProductoDatabase
 import es.etg.pmdm.productos.data.local.entities.ProductoEntity
 import es.etg.pmdm.productos.data.local.entities.ProveedorEntity
@@ -19,17 +26,22 @@ import retrofit2.converter.gson.GsonConverterFactory
 class MainActivity : AppCompatActivity() {
     private lateinit var productoService: ProductoAPIService
     private lateinit var proveedorService: ProveedorAPIService
+
     companion object {
         const val BASE_URL = "https://api.restful-api.dev/"
-        const val BASE_URL_DETALLE = "https://raw.githubusercontent.com/ManuelPavonBuendia/provedores/main/"
+        const val BASE_URL_DETALLE =
+            "https://raw.githubusercontent.com/ManuelPavonBuendia/provedores/main/"
         lateinit var database: ProductoDatabase
-        const val NOMBRE_BD="producto_database"
-        const val CARGADO_BD="Datos cargados desde la base de datos"
+        const val NOMBRE_BD = "producto_database"
+        const val CARGADO_BD = "Datos cargados desde la base de datos"
 
-        const val PROVEDOR_NO_ENCONTRADO="Proveedor no encontrado"
-        const val ERROR_CONSULTAR_SERVICIO="Error al consultar el servicio"
-        const val SIN_DATOS="Sin datos"
-        const val DATOS_CARGADOS="Datos cargados"
+        const val PROVEDOR_NO_ENCONTRADO = "Proveedor no encontrado"
+        const val ERROR_CONSULTAR_SERVICIO = "Error al consultar el servicio"
+        const val SIN_DATOS = "Sin datos"
+        const val DATOS_CARGADOS = "Datos cargados"
+        private val productosConProveedor = mutableListOf<ProductoConProveedor>()
+        private lateinit var adapter: ProductoAdapter
+
     }
 
     private lateinit var binding: ActivityMainBinding
@@ -38,13 +50,15 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        adapter = ProductoAdapter(this, productosConProveedor)
+        binding.listViewProductos.adapter = adapter
         productoService = createService(BASE_URL, ProductoAPIService::class.java)
         proveedorService = createService(BASE_URL_DETALLE, ProveedorAPIService::class.java)
         database = Room.databaseBuilder(
             applicationContext,
             ProductoDatabase::class.java,
             NOMBRE_BD
-            ).build()
+        ).build()
         //Implementación de pulsar el botón
         binding.btnConsultar.setOnClickListener {
             consultar(binding.edtIdInput.text.toString())
@@ -62,10 +76,7 @@ class MainActivity : AppCompatActivity() {
             runOnUiThread {
                 if (productoDb != null && proveedorDb != null) {
                     // Mostrar desde la BD
-                    binding.txtId.text = productoDb.id.toString()
-                    binding.txtNombre.text = productoDb.nombre
-                    binding.txtData.text = productoDb.descripcion
-                    binding.txtProveedor.text = proveedorDb.nombre
+                    agregarALista(productoDb.nombre, productoDb.descripcion, proveedorDb.nombre)
                     Toast.makeText(this@MainActivity, CARGADO_BD, Toast.LENGTH_SHORT).show()
                 } else {
                     // No está en la BD, hacemos la consulta a la API
@@ -84,23 +95,20 @@ class MainActivity : AppCompatActivity() {
 
             runOnUiThread {
                 if (producto != null) {
-                    val productoId = producto.id.toInt()
-                    binding.txtId.text = productoId.toString()
-                    binding.txtNombre.text = producto.nombre
-                    val productoDescripcion = producto.descripcion?.entries?.joinToString { "${it.key}: ${it.value}" } ?: SIN_DATOS
-                    binding.txtData.text = productoDescripcion
+                    val productoDescripcion = producto.descripcion
+                        ?.entries
+                        ?.joinToString { "${it.key}: ${it.value}" }
+                        ?: SIN_DATOS
                     val proveedorNombre = proveedor?.nombre ?: PROVEDOR_NO_ENCONTRADO
-                    binding.txtProveedor.text = proveedorNombre
-
-                    // Guardar en la BD
-                    guardar(productoId, producto.nombre, productoDescripcion, proveedorNombre)
+                    agregarALista(producto.nombre, productoDescripcion, proveedorNombre)
+                    guardar(producto.id.toInt(), producto.nombre, productoDescripcion, proveedorNombre)
                 } else {
-                    Toast.makeText(this@MainActivity, ERROR_CONSULTAR_SERVICIO, Toast.LENGTH_LONG).show()
+                    Toast.makeText(this@MainActivity, ERROR_CONSULTAR_SERVICIO, Toast.LENGTH_LONG)
+                        .show()
                 }
             }
         }
     }
-
 
 
     private fun <T> createService(baseUrl: String, serviceClass: Class<T>): T {
@@ -111,18 +119,17 @@ class MainActivity : AppCompatActivity() {
             .create(serviceClass)
     }
 
-    private fun guardar(id: Int, nombre: String, descripcion: String, proveedor: String){
+    private fun guardar(id: Int, nombre: String, descripcion: String, proveedor: String) {
         val productoDao = database.productoDao()
         val proveedorDao = database.proveedorDao()
 
-        var productoId: Long = 0 ;
-        val producto = ProductoEntity(id,nombre,descripcion)
+        val producto = ProductoEntity(id, nombre, descripcion)
 
         CoroutineScope(Dispatchers.IO).launch {
 
-            productoId = productoDao.insert(producto)
+            productoDao.insert(producto)
 
-            val proveedor = ProveedorEntity(0,proveedor,id)
+            val proveedor = ProveedorEntity(0, proveedor, id)
             // Insertamos el cliente
             proveedorDao.insert(proveedor)
             runOnUiThread {
@@ -131,4 +138,11 @@ class MainActivity : AppCompatActivity() {
         }
 
     }
+
+    private fun agregarALista(nombre: String, descripcion: String, proveedor: String) {
+        val item = ProductoConProveedor(nombre, descripcion, proveedor)
+        productosConProveedor.add(item)
+        adapter.notifyDataSetChanged()
+    }
+
 }
